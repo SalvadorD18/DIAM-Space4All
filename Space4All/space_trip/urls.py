2118ae9e7770a76a204153dac6bcc90035bfd482
Salvador Domingues <salvadordomingues@gmail.com>
2022-05-06 15:03:27 +0100

    favicon

--- Space4All/space_trip/urls.py
+++ Space4All/space_trip/urls.py
@@ -2,33 +2,37 @@ from django.urls import include, path
 from . import views
 from django.utils import timezone
 import datetime
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 # (. significa que importa views da mesma directoria)
 
 app_name = 'space_trip'
 
-urlpatterns = [ path("", views.index, name="index"),
-                path("<int:questao_id>", views.detalhe, name="detalhe"),
-                path("<int:questao_id>/resultados", views.resultados, name="resultados"),
-                path("<int:questao_id>/voto", views.voto, name="voto"),
-                path("view_questao_otimizada", views.view_questao_otimizada, name="view_questao_otimizada"),
-                path("register", views.register, name="register"),
-                path("logoutview", views.logoutview, name="logoutview"),
-                path("login", views.login, name="login"),
-                path("profile", views.profile, name="profile"),
-                path("<int:questao_id>/view_opcao_otimizada", views.view_opcao_otimizada, name="view_opcao_otimizada"),
-                path("<int:questao_id>/apagarquestao", views.apagarquestao, name="apagarquestao"),
-                path("<int:questao_id>/apagaropcao", views.apagaropcao, name="apagaropcao"),
-                path("fazer_upload", views.fazer_upload, name="fazer_upload"),
-                path("planets", views.planets, name="planets"),
-                path("moon", views.moon, name="moon"),
-                path("mercury", views.mercury, name="mercury"),
-                path("venus", views.venus, name="venus"),
-                path("earth", views.earth, name="earth"),
-                path("mars", views.mars, name="mars"),
-                path("jupiter", views.jupiter, name="jupiter"),
-                path("saturn", views.saturn, name="saturn"),
-                path("uranus", views.uranus, name="uranus"),
-                path("neptune", views.neptune, name="neptune"),
-                path("travelplanner", views.travelplanner, name="travelplanner"),
-                path("payment", views.payment, name="payment")
-                ]
+urlpatterns = [path("", views.index, name="index"),
+               path("<int:questao_id>", views.detalhe, name="detalhe"),
+               path("<int:questao_id>/resultados", views.resultados, name="resultados"),
+               path("<int:questao_id>/voto", views.voto, name="voto"),
+               path("view_questao_otimizada", views.view_questao_otimizada, name="view_questao_otimizada"),
+               path("register", views.register, name="register"),
+               path("logoutview", views.logoutview, name="logoutview"),
+               path("login", views.login, name="login"),
+               path("profile", views.profile, name="profile"),
+               path("<int:questao_id>/view_opcao_otimizada", views.view_opcao_otimizada, name="view_opcao_otimizada"),
+               path("<int:questao_id>/apagarquestao", views.apagarquestao, name="apagarquestao"),
+               path("<int:questao_id>/apagaropcao", views.apagaropcao, name="apagaropcao"),
+               path("fazer_upload", views.fazer_upload, name="fazer_upload"),
+               path("planets", views.planets, name="planets"),
+               path("moon", views.moon, name="moon"),
+               path("mercury", views.mercury, name="mercury"),
+               path("venus", views.venus, name="venus"),
+               path("earth", views.earth, name="earth"),
+               path("mars", views.mars, name="mars"),
+               path("jupiter", views.jupiter, name="jupiter"),
+               path("saturn", views.saturn, name="saturn"),
+               path("uranus", views.uranus, name="uranus"),
+               path("neptune", views.neptune, name="neptune"),
+               path("travelplanner", views.travelplanner, name="travelplanner"),
+               path("payment", views.payment, name="payment"),
+               path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('media/favicon.ico')))
+               ]
